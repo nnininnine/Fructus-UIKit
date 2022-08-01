6e7683d259a36906c1db08496867124f4aa4f621
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-08-01 14:20:34 +0700

    Initial Commit

---
+++ Fructus-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fructus-UIKit
+//
+//  Created by 7Peaks on 1/8/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
