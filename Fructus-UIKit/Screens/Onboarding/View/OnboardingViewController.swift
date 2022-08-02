12e2f4abab0429707b17b75d4e00292c20237738
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-08-02 09:47:32 +0700

    feat(onboarding): add navToHomeButton

--- Fructus-UIKit/Screens/Onboarding/View/OnboardingViewController.swift
+++ Fructus-UIKit/Screens/Onboarding/View/OnboardingViewController.swift
@@ -10,23 +10,54 @@ import UIKit
 
 class OnboardingViewController: UIViewController {
   // MARK: - Properties
+
+  private var navToHomeButton: UIButton = {
+    let button = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 40))
+    button.setTitle("😃 Start", for: .normal)
+    button.setTitleColor(.label, for: .normal)
+    button.translatesAutoresizingMaskIntoConstraints = false
+    
+    return button
+  }()
   
+  
+
   // MARK: - Init
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+
     setup()
   }
-  
+
   // MARK: - Methods
-  
+
   func setup() {
     setupUI()
   }
-  
+
   func setupUI() {
-    view.backgroundColor = .white
+    view.backgroundColor = .systemBackground
     title = "Onboarding"
+
+    // add button
+    navToHomeButton.addTarget(self, action: #selector(navToHome), for: .touchUpInside)
+    view.addSubview(navToHomeButton)
+    
+    // apply constraints
+    applyConstraints()
+  }
+  
+  private func applyConstraints() {
+    let navToHomeButtonConstraints = [
+      navToHomeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      navToHomeButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+    ]
+    
+    NSLayoutConstraint.activate(navToHomeButtonConstraints)
+  }
+
+  @objc func navToHome() {
+    navigationController?.setViewControllers([HomeViewController()], animated: true)
   }
 }
