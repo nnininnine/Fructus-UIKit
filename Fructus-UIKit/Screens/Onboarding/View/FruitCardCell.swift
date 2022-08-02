a174cb20d19bf3417ee8b431387a37783a69e431
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-08-02 12:03:27 +0700

    feat(onboarding): implement constraints for pageView, implement pageView delegate, datasource

--- Fructus-UIKit/Screens/Onboarding/View/FruitCardCell.swift
+++ Fructus-UIKit/Screens/Onboarding/View/FruitCardCell.swift
@@ -14,16 +14,6 @@ class FruitCardCell: UICollectionViewCell {
   
   private var fruit: Fruit?
 
-  // MARK: - Init
-
-  override init(frame: CGRect) {
-    super.init(frame: frame)
-  }
-  
-  required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-
   // MARK: - Methods
   func setup() {
     setupUI()
--- Fructus-UIKit/Screens/Onboarding/View/OnboardingViewController.swift
+++ Fructus-UIKit/Screens/Onboarding/View/OnboardingViewController.swift
@@ -12,10 +12,16 @@ class OnboardingViewController: UIViewController {
   // MARK: - Properties
 
   private var pageView: UICollectionView = {
-    let collectionView: UICollectionView = .init()
+    let collectionView: UICollectionView = .init(frame: .zero, collectionViewLayout: .init())
 
+    let layout: UICollectionViewFlowLayout = .init()
+    layout.scrollDirection = .horizontal
+    layout.itemSize = UIScreen.main.bounds.size
+
+    collectionView.collectionViewLayout = layout
     collectionView.isPagingEnabled = true
     collectionView.translatesAutoresizingMaskIntoConstraints = false
+    collectionView.backgroundColor = .red
 
     return collectionView
   }()
@@ -30,19 +36,19 @@ class OnboardingViewController: UIViewController {
 
   override func viewDidLayoutSubviews() {
     super.viewDidLayoutSubviews()
-
-    pageView.frame = view.bounds
   }
 
   // MARK: - Methods
 
   func setup() {
     setupUI()
+    setupPageView()
   }
 
   func setupUI() {
     view.backgroundColor = .systemBackground
     title = "Onboarding"
+    navigationController?.navigationBar.isHidden = true
 
     // add fruit card
     view.addSubview(pageView)
@@ -51,13 +57,41 @@ class OnboardingViewController: UIViewController {
     applyConstraints()
   }
 
+  func setupPageView() {
+    pageView.delegate = self
+    pageView.dataSource = self
+    pageView.register(FruitCardCell.self, forCellWithReuseIdentifier: FruitCardCell.identifier)
+  }
+
   private func applyConstraints() {
-//    NSLayoutConstraint.activate()
+    let pageViewConstraints = [
+      pageView.widthAnchor.constraint(equalToConstant: view.bounds.width),
+      pageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+      pageView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
+    ]
+    
+    NSLayoutConstraint.activate(pageViewConstraints)
   }
 
   @objc func navToHome() {
     UserDefaults().set(true, forKey: UserDefaultsKey.isOnboarding.rawValue)
 
     navigationController?.setViewControllers([HomeViewController()], animated: true)
+  }
+}
+
+// MARK: - UICollectionViewDelegate, UICollectionViewDataSource
+
+extension OnboardingViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 0
+  }
+
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FruitCardCell.identifier, for: indexPath) as? FruitCardCell else { return UICollectionViewCell() }
+
+    cell.configure(with: fruitsData[indexPath.row])
+
+    return cell
   }
 }
