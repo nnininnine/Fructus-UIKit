c14d64dc1cf0425e81f030ebc11629f3b99363a7
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-08-02 16:40:47 +0700

    feat(home): create FruitCell class

---
+++ Fructus-UIKit/Screens/Home/View/FruitCell.swift
@@ -0,0 +1,18 @@
+//
+//  FruitCell.swift
+//  Fructus-UIKit
+//
+//  Created by 7Peaks on 2/8/2565 BE.
+//
+
+import UIKit
+
+class FruitCell: UITableViewCell {
+  // MARK: - Properties
+
+  static let identifier = "FruitCell"
+
+  // MARK: - Methods
+
+  override func setSelected(_ selected: Bool, animated: Bool) {}
+}
