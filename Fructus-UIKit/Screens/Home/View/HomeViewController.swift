6145c20ea4370f068651bfcfa95c1ef75821a569
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-08-02 09:28:07 +0700

    feat: create HomeViewController class

---
+++ Fructus-UIKit/Screens/Home/View/HomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HomeViewController.swift
+//  Fructus-UIKit
+//
+//  Created by 7Peaks on 2/8/2565 BE.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+  // MARK: - Properties
+
+  // MARK: - Init
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    setup()
+  }
+
+  // MARK: - Methods
+
+  func setup() {
+    setupUI()
+  }
+
+  func setupUI() {
+    view.backgroundColor = .white
+    title = "Home"
+  }
+}
