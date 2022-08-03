dd2728be97fceebe60fa084187e4adaf77595f90
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-08-03 10:48:18 +0700

    feat: create SettingsViewController class

---
+++ Fructus-UIKit/Screens/Settings/View/SettingsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SettingsViewController.swift
+//  Fructus-UIKit
+//
+//  Created by 7Peaks on 3/8/2565 BE.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+  // MARK: - Properties
+  
+  // MARK: - Subviews
+  
+  // MARK: - LifeCycles
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+  
+  // MARK: - Methods
+}
