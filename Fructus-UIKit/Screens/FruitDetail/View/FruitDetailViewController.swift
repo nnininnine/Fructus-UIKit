60ee30c5c33f86ab1e3e84a4b7048fac2a4548e7
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-08-03 11:09:17 +0700

    feat(detail): create FruitDetailViewController class

---
+++ Fructus-UIKit/Screens/FruitDetail/View/FruitDetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FruitDetailViewController.swift
+//  Fructus-UIKit
+//
+//  Created by 7Peaks on 3/8/2565 BE.
+//
+
+import UIKit
+
+class FruitDetailViewController: UIViewController {
+  // MARK: - Properties
+
+  // MARK: - Subviews
+
+  // MARK: - LifeCycles
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  // MARK: - Methods
+}
